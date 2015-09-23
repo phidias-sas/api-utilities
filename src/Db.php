1403501164d5fc7b006da76232789177e6449395
Santiago Cortes <santiago.cortes@phidias.com.co>
2015-09-23 15:18:30 -0500

    initial commit

---
+++ src/Db.php
@@ -0,0 +1,45 @@
+<?php
+namespace Phidias\Api\Utilities;
+
+class Db
+{
+    public static function filterCollection($collection, $request)
+    {
+        $parameters = $request->getQueryParams();
+        
+        if (isset($parameters["limit"])) {
+            $collection->limit($parameters["limit"]);
+        }
+
+        if (isset($parameters["page"])) {
+            $collection->page($parameters["page"]);
+        }
+
+        if (isset($parameters["order"])) {
+
+            $sortAttribute = $parameters["order"];
+            $isDescending  = false;
+            $firstChar     = substr($sortAttribute, 0, 1);
+
+            if ($firstChar == "+" || $firstChar == "-") {
+                $sortAttribute = substr($sortAttribute, 1);
+                $isDescending  = $firstChar == "-";
+            }
+
+            $collection->orderBy($sortAttribute, $isDescending);
+        }
+
+    }
+
+    public static function searchCollection($collection, $request)
+    {
+        $parameters           = $request->getQueryParams();
+        $searchableAttributes = func_get_args();
+        unset($searchableAttributes[0]);
+        unset($searchableAttributes[1]);
+
+        if (isset($parameters["q"])) {
+            $collection->search($parameters["q"], $searchableAttributes);
+        }
+    }
+}
